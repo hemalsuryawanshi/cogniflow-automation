609d135480a52dfb054c7efc13bf726a9c13c589
Hemal Kiran suryawanshi <156309748+hemalsuryawanshi@users.noreply.github.com>
2026-02-18 19:44:58 +0530

    Update config.py

--- gateway/app/config.py
+++ gateway/app/config.py
@@ -1,16 +1,26 @@
 import os
-from dotenv import load_dotenv
 
-# Load .env file
-load_dotenv()
+# Railway automatically provides environment variables
+# No need for dotenv in production
 
-OCR_URL = os.getenv("OCR_URL")
-PREPROCESS_URL = os.getenv("PREPROCESS_URL")
-EXTRACT_URL = os.getenv("EXTRACT_URL")
-CONFIDENCE_URL = os.getenv("CONFIDENCE_URL")
+#OCR_URL = os.environ.get("OCR_URL")
+#PREPROCESS_URL = os.environ.get("PREPROCESS_URL")
+#EXTRACT_URL = os.environ.get("EXTRACT_URL")
+#CONFIDENCE_URL = os.environ.get("CONFIDENCE_URL")
+OCR_URL = "https://generous-passion-production-5ad5.up.railway.app/ocr"
+PREPROCESS_URL = "https://remarkable-inspiration-production-017e.up.railway.app/preprocess"
+EXTRACT_URL = "https://blissful-flow-production-ba39.up.railway.app/extract"
+CONFIDENCE_URL = "https://resilient-playfulness-production-4f84.up.railway.app/confidence"
 
-# Default to SQLite if DATABASE_URL not set
-DATABASE_URL = os.getenv(
+
+# Fallback database
+DATABASE_URL = os.environ.get(
     "DATABASE_URL",
     "sqlite:///./cogniflow.db"
 )
+
+# Debug print (VERY IMPORTANT for checking Railway variables)
+print("OCR_URL:", OCR_URL)
+print("PREPROCESS_URL:", PREPROCESS_URL)
+print("EXTRACT_URL:", EXTRACT_URL)
+print("CONFIDENCE_URL:", CONFIDENCE_URL)
